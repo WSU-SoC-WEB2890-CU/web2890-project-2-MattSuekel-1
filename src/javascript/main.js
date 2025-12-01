ab845dc1bb2efb5f71ba336fdee7c39a9a1d25a5
MattSuekel <matthew.suekel@gmail.com>
2025-12-01 00:13:04 -0700

    more set up, nav and footer partials cuz i dont wanna redo it a bunch of times

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,1 +1,11 @@
 import '../style.css'
+
+
+import { createIcons, Menu, X } from 'lucide';
+
+createIcons({
+    icons: {
+        Menu,
+        X,
+    }
+});
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import htmlInject from 'vite-plugin-html-inject';
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
-})
+  plugins: [
+    tailwindcss(),
+    htmlInject()
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "about/index.html",
+        contact: "contact/index.html",
+        work: "work/index.html",
+      },
+    },
+  },
+});
